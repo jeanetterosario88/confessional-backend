9ea2917efcd7cdd08132a4810a1a4c5f8fffda50
jeanetterosario88 <jeanetterosario88@gmail.com>
2021-01-18 15:08:48 -0500

    added routes and controllers

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -14,12 +14,20 @@ class SecretsController < ApplicationController
 
     def create
         secret = Secret.new(secret_params)
+        secret.likes = 0
         if secret.save
             render json: secret.to_json(:include => :comments)
         else
             render json: {errors: secret.errors.full_messages}
         end
     end
+
+    def update
+        secret = Secret.find(params[:id])
+        upvoted_likes = secret.likes + 1
+        secret.update(likes: upvoted_likes)
+        render json: secret.to_json(:include => :comments)
+      end
     
     
       private
